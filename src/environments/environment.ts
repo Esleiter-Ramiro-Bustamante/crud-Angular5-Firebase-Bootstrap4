2d2e49b70412e5d698e487833e58153d73bfe519
Esleiter-Ramiro-Bustamante <esletutuns0307@gmail.com>
2018-03-13 02:25:24 -0600

    Crud-Angular5-Bootstrap4-v1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey:  "AIzaSyC3Hv7HNa2j-g4pgbB-Z1QObcKKxlAtUFQ",
+    authDomain: "angular-crudfirebase.firebaseapp.com",
+    databaseURL: "https://angular-crudfirebase.firebaseio.com",
+    projectId: "angular-crudfirebase",
+    storageBucket: "angular-crudfirebase.appspot.com",
+    messagingSenderId: "722694667782"
+  }
 };
